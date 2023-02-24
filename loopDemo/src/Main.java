fb44027f81e281d8e735c5837301cccd5b2854db
sahinemin <emin.sahin@agu.edu.tr>
2023-02-24 17:37:42 +0300

    for and while loop example

---
+++ loopDemo/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i+=2) {
+            System.out.println(i);
+        }
+        System.out.println("For Döngüsü bitti");
+        int i=1;
+        while (i<10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("While Döngüsü bitti");
+    }
+}
