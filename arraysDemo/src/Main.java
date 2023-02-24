4adbed0f04cd200f02eabd4ab630a16126adfc95
sahinemin <emin.sahin@agu.edu.tr>
2023-02-24 17:44:19 +0300

    array for and for each example

---
+++ arraysDemo/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String [] ogrenciler = new String[4];
+        ogrenciler[0]="Engin";
+        ogrenciler[1]="Derin";
+        ogrenciler[2]="Selin";
+        ogrenciler[3]="Ahmet";
+        for (int i = 0; i < ogrenciler.length; i++) {
+            System.out.println(ogrenciler[i]);
+        }
+        for (String ogrenci:
+             ogrenciler) {
+            System.out.println(ogrenci);
+        }
+    }
+}
