8231ddfd1804f1f9bd8d56827a8f916d4ce17c2c
sahinemin <emin.sahin@agu.edu.tr>
2023-02-25 12:50:00 +0300

    the difference between reference and value data types example

---
+++ javaOOP/classes/src/CustomerManager.java
@@ -0,0 +1,13 @@
+public class CustomerManager {
+    public void Add() {
+        System.out.println("Müsteri Eklendi");
+    }
+
+    public void Remove() {
+        System.out.println("Müsteri Silindi");
+    }
+
+    public void Update() {
+        System.out.println("Müsteri Güncellendi");
+    }
+}
